837070fb68ed4f1a77412981e8828723f183d2f7
yoyo.sincerely <yoyo.cincerely@gmail.com>
2017-09-15 18:56:10 +0800

    done

--- SongPoem/SongPoem.py
+++ SongPoem/SongPoem.py
@@ -26,7 +26,8 @@ def parse_html(html):
     # f.write(html)
     soup = BeautifulSoup(html, "html.parser")
     Poem_list_soup = soup.find('div', attrs={'class': 'typecont'})
-    Poem_href_list = {}
+    Poem_href_list = []
+    Poem_name_list = []
     # f.write(Poem_list_soup)
     # print(Poem_list_soup.prettify())
 
@@ -40,19 +41,39 @@ def parse_html(html):
         Poem_href = Poem_a.attrs['href']
         Poem_name = Poem_a.getText()
         # print(Poem_href, Poem_name)
-        Poem_href_list.append(proto + '://' + domain + Poem_href)
+        Poem_Url = proto + '://' + domain + Poem_href
+        # print(Poem_Url)
+        Poem_name_list.append(Poem_name)
+        Poem_href_list.append(Poem_Url)
     return Poem_href_list
 
 def getPoem(Poem_href_list):
-    for Poem_Url in Poem_href_list:
-        Poem_html = download_page(Poem_Url)
+    # for Poem_Url in Poem_href_list:
+    #     Poem_html = download_page(Poem_Url)
+    Poem_data_list = []
 
+    # Poem_html = download_page(Poem_href_list[0])
+    for Poem_URL in Poem_href_list:
+        print(Poem_URL)
+        Poem_html = download_page(Poem_URL)
+        Poem_soup = BeautifulSoup(Poem_html, "html.parser")
+        Poem_list = Poem_soup.find('div', attrs = {'class' : 'contson'})
+        # f = open(CODE_FILE, 'wb')
+        # f.write(Poem_list)
+        # f.close()
+        for Poem_data in Poem_list.find_all('p'):
+            Poem_data_list.append(Poem_data.getText())
+            # print(Poem_data.getText())
+        # print(Poem_list.encode(gbk))
+    return Poem_data_list
 
 def ok():
     url = DOWNLOAD_URL
     html = download_page(url)
-    parse_html(html)
-    # with codecs.open(CODE_FILE, 'wb', encoding='utf-8') as fp:
+    getPoem(parse_html(html))
+    with codecs.open(CODE_FILE, 'wb', encoding='utf-8') as fp:
+        for Poem in getPoem(parse_html(html)):
+            fp.write(u'{Poem}\n'.format(Poem=''.join(Poem)))
     #     while url:
     #         html = download_page(url)
     #         fp.write(html)
